a976c194361b2e507153f5608e80932cfc6b68d4
Venkatesh Gopalakrishnan <96gvenkatesh@gmail.com>
2019-02-25 19:41:13 +0530

    same user storing again - fixing

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const mongoose = require('mongoose')
 const keys = require("./config/keys");
 
-
+// require models before passport, else schema wont be registered
+require('./models/Users')
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI)
 
 const app = express();
 require('./routes/authRoutes')(app)
-// require models before passport, else schema wont be registered
-require('./models/Users')
-require('./services/passport');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -3,38 +3,54 @@ const mongoose = require('mongoose')
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const keys = require("../config/keys");
-const Users = mongoose.model('Users');
+const User = mongoose.model('Users');
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback"
-      },
-      (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-        // new Users({
-        //   googleID: profile.id,
-        // }).save();
-      }
-    )
-  );
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // console.log("access token", accessToken);
+      // console.log("refresh token", refreshToken);
+      // console.log("profile", profile);
+      User.findOne({ id: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // do nothing as the user already exists
+          }
+          else {
+            //user id not present. Create a new user with this id
+            new User({ id: profile.id }).save();
+          }
+        })
+    }
+  )
+);
 
-  passport.use(
-    new LinkedInStrategy(
-      {
-        clientID: keys.linkedinClientID,
-        clientSecret: keys.linkedinClientSecret,
-        callbackURL: "/auth/linkedin/callback"
-      },
-      (accessToken, refreshToken, profile, done) => {
-        console.log("access token", accessToken);
-        console.log("refresh token", refreshToken);
-        console.log("profile", profile);
-        new Users({
-          linkedinID: profile.id,
-        }).save();
-      }
-    )
-  );
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: keys.linkedinClientID,
+      clientSecret: keys.linkedinClientSecret,
+      callbackURL: "/auth/linkedin/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // console.log("access token", accessToken);
+      // console.log("refresh token", refreshToken);
+      // console.log("profile", profile);
+      User.findOne({ id: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // do nothing as the user already exists
+          }
+          else {
+            //user id not present. Create a new user with this id
+            new User({ id: profile.id }).save();
+          }
+        })
+    }
+  )
+);
