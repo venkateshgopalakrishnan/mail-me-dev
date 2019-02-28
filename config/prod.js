9aaa2483c860f6479fec72d87a550d43fdac0438
Venkatesh Gopalakrishnan <96gvenkatesh@gmail.com>
2019-02-28 14:51:09 +0530

    production keys written

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    linkedinClientID: process.env.LINKEDIN_CLIENT_ID,
+    linkedinClientSecret: process.env.lINKEDIN_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
